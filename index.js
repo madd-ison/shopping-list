4ed7afadf742bfc4c0f8ba308c4439f70cc1aa36
madd-ison <65075658+madd-ison@users.noreply.github.com>
2020-05-20 13:01:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+$(document).ready(function() {
+$("#js-shopping-list-form").on('.submit', function(e){
+	var text = $('#shopping-list-entry').val();
+  $('.shoppingList').append($('<li>').append(text));
+  $('#shopping-list-entry').val('');
+  e.preventDefault();
+});
+});
 
+$('.shopping-item-delete').on('click', 'li', function(){
+	$(this).remove();
+});
