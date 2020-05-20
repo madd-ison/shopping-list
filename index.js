cc3564f99a335ea38cf6bda0d69ed66384f18444
madd-ison <65075658+madd-ison@users.noreply.github.com>
2020-05-20 16:42:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 $(document).ready(function() {
-$("#js-shopping-list-form").on('submit', function (event){
-  let text = $("#shopping-list-entry").val();
-  $(".shopping-list").append('<li>' + text + '</li>');
-  event.preventDefault();
-});
-});
+  $('#js-shopping-list-form').submit(function(event) {
+    event.preventDefault();
+    $('.shopping-list').append(
+      '<li>' +
+        '<span class="shopping-item">' + $("#shopping-list-entry").val() + '</span>' +
+        '<div class="shopping-item-controls">' +
+          '<button class="shopping-item-toggle">' +
+            '<span class="button-label">check</span>' +
+          '</button>' +
+          '<button class="shopping-item-delete">' +
+            '<span class="button-label">delete</span>' +
+          '</button>' +
+        '</div>' +
+      '</li>'
+    );
+    $(this)[0].reset(); 
+  });
+  $('.shopping-list').on('click', '.shopping-item-delete', function(){
+    $(this).closest('li').remove();
+  });
+  $('.shopping-list').on('click', '.shopping-item-toggle', function(){
+    $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+  });
+
+})
